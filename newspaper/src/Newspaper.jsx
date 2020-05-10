30de3e1e5750dda3728db842e1947580d37fa3ca
VAIBHAV SHUKLA <shuklavaibhav0306@gmail.com>
2020-05-10 22:22:08 +0530

    updated comments

--- newspaper/src/Newspaper.jsx
+++ newspaper/src/Newspaper.jsx
@@ -5,6 +5,9 @@ function Newspaper(){
     const [state,setState]=React.useState([]);
     const allnews=[];
     const allscore={};
+
+    //--------------------------------Get Request for getting stories------------------------------------------//
+
     useEffect(()=>{
         fetch("https://hacker-news.firebaseio.com/v0/askstories.json?print=prett")
       .then(res=>res.json())
@@ -21,21 +24,23 @@ function Newspaper(){
       )
     },[]);
 
+    //--------------------------------Get Request to get details about individual story ------------------------------------------//
+
     async function getContent(val,i, len){
         await fetch(`https://hacker-news.firebaseio.com/v0/item/${val}.json?print=pretty`)
         .then(res=>res.json())
         .then(result=> {
             var title=result.title.replace("Ask HN: ", "");
             var score=result.score;
-
-            console.log(score);
             allnews.push({title:title,content:result.text,score:score});
             if(i===len-1){
-                console.log("setting state")
                 setState(allnews);
             }
         });
     }
+
+    //--------------------------------Title font size according to score of that story------------------------------------------//
+
     var count=25,i;
     for(i in state){
       allscore[state[i].score]="";
